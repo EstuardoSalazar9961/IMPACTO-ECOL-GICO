61e473913bf3057efc29e6e30b47a13f888f18b0
EstuardoSalazar9961 <149229123+EstuardoSalazar9961@users.noreply.github.com>
2023-11-26 00:36:19 -0500

    iteraciones arraylist

--- CalculoImpactoEcologico/src/calculoimpactoecologico/CalculoImpactoEcologico.java
+++ CalculoImpactoEcologico/src/calculoimpactoecologico/CalculoImpactoEcologico.java
@@ -5,6 +5,8 @@
  */
 package calculoimpactoecologico;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author CompuStore
@@ -16,6 +18,25 @@ public class CalculoImpactoEcologico {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        // Crear objetos de cada clase
+        Edificio edificio = new Edificio("Edificio Dorado", 2000);
+        Auto auto = new Auto("Mazda BT-50", 7);
+        Bicicleta bicicleta = new Bicicleta("Montañera");
+
+        // Crear ArrayList para almacenar objetos que implementan ImpactoEcologico
+        ArrayList<ImpactoEcologico> listaImpacto = new ArrayList<>();
+        listaImpacto.add(edificio);
+        listaImpacto.add(auto);
+        listaImpacto.add(bicicleta);
+
+        // Iterar a través de la lista e imprimir el impacto ecológico
+        for (ImpactoEcologico impacto : listaImpacto) {
+            System.out.println(impacto.getIdentificacion());
+            System.out.println("Impacto ecológico: " + impacto.obtenerImpactoEcologico());
+            System.out.println("------------------------");
+        }
+
     }
-    
 }
+    
