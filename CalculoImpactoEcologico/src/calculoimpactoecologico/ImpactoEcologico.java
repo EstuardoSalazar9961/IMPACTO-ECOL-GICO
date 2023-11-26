c839b417a4329d0663963a6bfd9cc1c2f12f2342
EstuardoSalazar9961 <149229123+EstuardoSalazar9961@users.noreply.github.com>
2023-11-26 00:28:20 -0500

    impacto ecologico
    interfaz

---
+++ CalculoImpactoEcologico/src/calculoimpactoecologico/ImpactoEcologico.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculoimpactoecologico;
+
+/**
+ *
+ * @author CompuStore
+ */
+public abstract class ImpactoEcologico {
+    public abstract double obtenerImpactoEcologico();
+    public abstract String getIdentificacion();
+}
